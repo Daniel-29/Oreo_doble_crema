5b6fb4c7a2123fa250308fadeb2ee543262844af
Mystogan <39338987+Daniel-29@users.noreply.github.com>
2020-06-14 15:31:43 -0600

    Add: classes for gameplay were created

---
+++ Arkanoid/Arkanoid/Core/Gameplay/Player.cs
@@ -0,0 +1,14 @@
+﻿namespace Arkanoid.Core.Gameplay
+{
+  public class Player
+  {
+    public int platformX;
+    public int platformY;
+    public int ballX;
+    public int ballY;
+    public int dirX = 0;
+    public int dirY = 0;
+
+    public int platformAnnimationFrame = 3; // max = 3;
+  }
+}
