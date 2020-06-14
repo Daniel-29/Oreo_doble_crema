8df111f36f1732906261462a7bb3c8d70ad45916
Mystogan <39338987+Daniel-29@users.noreply.github.com>
2020-06-13 21:55:59 -0600

    Modify:User control "Record" was modified and the view is ended.

--- Arkanoid/Arkanoid/Record.cs
+++ Arkanoid/Arkanoid/Record.cs
@@ -1,12 +1,27 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace Arkanoid
 {
-    public partial class Record : UserControl
+  public partial class Record : UserControl
+  {
+    private Form frm;
+    private const string path = "../../Resources/";
+    private Image title = new Bitmap(path+"top10.png");
+    public Record(Form frm)
     {
-        public Record(Form frm)
-        {
-            InitializeComponent();
-        }
+      this.frm = frm;
+      InitializeComponent();
+      ptb_title.SizeMode = PictureBoxSizeMode.StretchImage;
+      ptb_title.Image = title;
+      cargar();
     }
+
+    public void cargar()
+    {
+      var gm = new Core.Controller.Game();
+      dgv_records.DataSource = gm.viewTopPlayers();
+    }
+    
+  }
 }
