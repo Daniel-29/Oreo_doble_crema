9a4090d92c907be8343fddb882db3d835f1b9e85
EstefanyAguirre12 <estefanyaguirre1200@hotmail.com>
2020-06-22 15:38:20 -0600

    Add: exception classes

---
+++ Arkanoid/Arkanoid/Core/Controller/LessThanOneValueException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Arkanoid.Core.Controller
+{
+    public class LessThanOneValueException : Exception
+    {
+        public LessThanOneValueException(string message):base(message){ }
+    }
+}
---
+++ Arkanoid/Arkanoid/Core/Controller/NoRemainingLifesException.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Arkanoid.Core.Controller
+{
+    public class NoRemainingLifesException : Exception
+    {
+        public NoRemainingLifesException(string message):base(message){ }
+    }
+}
